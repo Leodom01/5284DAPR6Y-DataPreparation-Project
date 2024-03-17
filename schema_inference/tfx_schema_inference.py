e66c2c3731bd94acd8e9f7e3977909c5d1212822
Duc <nguyenminhducci@gmail.com>
2024-03-17 21:50:20 +0100

    Count occurences of relevant data for batches

--- schema_inference/tfx_schema_inference.py
+++ schema_inference/tfx_schema_inference.py
@@ -1,25 +1,41 @@
 import random
+from collections import Counter
 
 import pandas as pd
 import tensorflow_data_validation as tfdv
+from tensorflow_metadata.proto.v0 import schema_pb2
+
+from schema_inference.helper import schema_pb2obj, Schema
 
 
 class SchemaInference:
-    BATCH_SIZE = 5000
+    BATCH_SIZE = 100
 
     def __init__(self, path):
         self.data = pd.read_csv(path)
         self.batch_indices_list = []
-        self.schemas = []
+        self.feature_count = {column: Counter() for column in self.data.columns}
+        self.domain_count = {column: Counter() for column in self.data.columns}
         self.aggregated_schema = None
         self.anomalies = []
 
-    def partition_data(self, seed=None):
-        if seed is not None:
-            random.seed(seed)
+    def _count_relevant_data(self, schema: Schema):
+        for feature in schema.features:
+            name = feature.name
+
+            if name in self.data.columns:
+                self.feature_count[name][feature.type] += 1
+                self.feature_count[name]['presence_min_fraction'] += 1 if feature.presence.min_fraction else 0
+                self.feature_count[name]['presence_min_count'] += 1 if feature.presence.min_count > 0 else 0
+
+        for domain in schema.domains:
+            self.domain_count[domain.name][frozenset(domain.values)] += 1
+
+    def _partition_data(self, seed=42):
+        random.seed(seed)
 
         indices = list(self.data.index)
-        random.shuffle(indices)  # Shuffle the indices randomly
+        random.shuffle(indices)
         self.batch_indices_list = []
 
         while indices:
@@ -31,20 +47,28 @@ class SchemaInference:
                 self.batch_indices_list.append(indices)
                 indices = []
 
-        return
-
-    def infer_schema(self):
+    def _infer_schemas_from_batches(self):
         # Infer the schema
         for batch_indices in self.batch_indices_list:
             batch = self.data.iloc[batch_indices]
             stats = tfdv.generate_statistics_from_dataframe(batch)
-        # stats = tfdv.generate_statistics_from_csv(path)
-            tfdv.visualize_statistics(stats)
-        # print(stats)
-            schema = tfdv.infer_schema(stats)
-            return schema
+            schema_pb = tfdv.infer_schema(stats)
+            schema = schema_pb2obj(schema_pb)
+            self._count_relevant_data(schema)
 
-        #return
+    def _aggregate_schemas(self):
+        ...
+
+    def infer_schema(self) -> schema_pb2.Schema:
+        self._partition_data()
+        self._infer_schemas_from_batches()
+        self._aggregate_schemas()
+        return self.aggregated_schema
+
+    def detect_anomalies(self):
+        ...
+
+        # return
 
 
 # Example usage
@@ -54,7 +78,8 @@ if __name__ == '__main__':
     data_path = '../datasets/salaries/ds_salaries.csv'
 
     schema_inferer = SchemaInference(data_path)
-    schema_inferer.partition_data(seed=42)
-    schema = schema_inferer.infer_schema()
-    # print(schema)
-    # tfdv.display_schema(schema=schema)
+    schema_inferer._partition_data(seed=42)
+    aggregated_schema = schema_inferer.infer_schema()
+    # print(aggregated_schema)
+    # print(schema_pb2obj(aggregated_schema))
+    # tfdv.display_schema(schema=aggregated_schema)
