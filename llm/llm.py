0cc092e9b5ae860832c526346ab1123b0033c6fe
badbubble <liuhanjun369@gmail.com>
2024-03-18 23:52:20 +0100

    add llama2:70b and dolphin-mixtral:8x7b modules

--- llm/llm.py
+++ llm/llm.py
@@ -1,19 +1,44 @@
 import json
-
+import pandas as pd
 from openai import OpenAI
+from collections import Counter
+import requests
+
+OLLAMA_URL = "http://localhost:11434/api/generate"
 
 
 class LLM:
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str) -> None:
         self.client = OpenAI(api_key=api_key)
 
+    def explore_data_types(self, data: pd.DataFrame, sample_size: int = 5, batch_size: int = 3, model_name: str = "chatgpt") -> dict[str, str]:
+        result = dict()
+        column_names: list[str] = data.columns
+
+        for column_name in column_names:
+            result_list = []
+            for _ in range(batch_size):
+                random_values = data[column_name].sample(n=sample_size)
+                if model_name == "chatgpt":
+                    tmp_result = self.chatgpt(column_name, random_values)
+                elif model_name == "mixtral":
+                    tmp_result = self.ollama(column_name, random_values, "dolphin-mixtral:8x7b")
+                else:
+                    tmp_result = self.ollama(column_name, random_values, "llama2:70b")
+                result_list.append(tmp_result)
+            counter = Counter(result_list)
+            result[column_name] = counter.most_common(1)[0][0]
+
+        return result
+        
     def chatgpt(self, column_name: str, values: list[str]) -> str:
+        values = [str(i) for i in values]
         completion = self.client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "In the following prompts I will give you a column name and some values."
                                               " Tell me whether the type is either DATE, TIME, DATETIME, URL, EMAIL, "
-                                              "(INT, FLOAT), JSON, BOOLEAN, NETWROK ADDDRESSES or XML. "
+                                              "INT, FLOAT, JSON, BOOLEAN, NETWROK ADDDRESSES, XML, CATEGORY or TEXT. "
                                               "Only decide for a data type if most samples fit. "
                                               "I will provide you with sample values from this column."
                                               " Give me the answer in JSON format with the"
@@ -22,39 +47,39 @@ class LLM:
             ],
             response_format={"type": "json_object"}
         )
+
         result_json = json.loads(completion.choices[0].message.content)
 
         return result_json["type"]
 
+    def ollama(self, column_name: str, values: list[str], model_name: str = "dolphin-mixtral:8x7b") -> str:
+        values = [str(i) for i in values]
+        prompt = ("In the following prompts I will give you a column name and some values."
+            " Tell me whether the type is either DATE, TIME, DATETIME, URL, EMAIL, INT, FLOAT, JSON, BOOLEAN,"
+            " NETWROK ADDDRESSES, XML, CATEGORY or TEXT. Only decide for a data type if most samples fit."
+            " I will provide you with sample values from this column. Only give me the answer in one word format "
+            "for example 'DATE' (required) \n"
+            "------\n"
+            f"column name: {column_name}, values: {', '.join(values)}"
+            )
+        
+        data = {
+            "model": model_name,
+            "prompt": prompt,
+            "stream": False
+        }
+        response_json = requests.post(OLLAMA_URL, json=data).json()        
+        return response_json["response"].strip()
+
 
 if __name__ == "__main__":
     llm = LLM(api_key="")
-    column_type = llm.chatgpt(
-        column_name="time",
-        values=["2024-03-03 08:30:00", "2024-03-03 12:45:00", "luke.hall@example.com", "2024-03-03 15:20:00",
-                "2024-03-04 09:00:00"]
-    )
-    print(column_type)
+    data = pd.read_csv("datasets/carprices/car_prices.csv", sep=",")
 
-    column_type = llm.chatgpt(
-        column_name="time",
-        values=["emma.martin@example.com", "luke.hall@example.com", "chloe.davis@example.com",
-                "ethan.jackson@example.com",
-                "2024-03-03 08:30:00",
-                "ava.robinson@example.com",
-                ]
-    )
-    print(column_type)
+    print("[+] The result of ChatGPT 3.5")
+    print(llm.explore_data_types(data=data, model_name="chatgpt"))
+    print("[+] The result of llama2:70b")
+    print(llm.explore_data_types(data=data, model_name="llama2"))
+    print("[+] The result of dolphin-mixtral:8x7b")
+    print(llm.explore_data_types(data=data, model_name="mixtral"))   
 
-    column_type = llm.chatgpt(
-        column_name="time",
-        values=["https://www.example.com/page?category=electronics&product=laptop",
-                "https://www.example.com/blog/article?id=123&category=technology",
-                "https://www.example.com/shop/product?product_id=456&color=blue",
-                "https://www.example.com/page?category=clothing&gender=male&size=XL",
-                "https://www.example.com/blog/post?id=789&tag=tips&source=twitter",
-                "2024-03-03 08:30:00",
-                "ava.robinson@example.com",
-                ]
-    )
-    print(column_type)
